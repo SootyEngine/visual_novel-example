79c7c376572535b869326388b96b05e5ad5e637a
teebarjunk <teebarjunk@github.com>
2022-03-20 21:23:11 -0400

    initial

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Sooty - Example VS
 A visual story/novel, built with [Sooty](https://github.com/teebarjunk/sooty) and [Godot 4](https://godotengine.org/).
 
+![](README/preview.png)
+
 Early alpha. About as unstable as Godot 4.
--- addons/simple_vn/ui/ui_overdraw.gd
+++ addons/simple_vn/ui/ui_overdraw.gd
@@ -6,6 +6,8 @@ func _process(delta: float) -> void:
 	update()
 
 func _draw() -> void:
+	return
+	
 	var h := DialogueStack._halting_for
 	if len(h):
 		draw_string(FONT, Vector2(16, 24), "Waiting for:", HORIZONTAL_ALIGNMENT_LEFT, -1, 24, Color.SLATE_GRAY, 4, Color.SLATE_GRAY.darkened(.33))
