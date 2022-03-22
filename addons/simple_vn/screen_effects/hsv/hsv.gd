1ff1087615b2f24f9a1021e12d6b243e01f2589a
teebarjunk <teebarjunk@github.com>
2022-03-21 22:06:42 -0400

    bloom effect

--- addons/simple_vn/screen_effects/hsv/hsv.gd
+++ addons/simple_vn/screen_effects/hsv/hsv.gd
@@ -1,22 +1,15 @@
 @tool
-extends Node
+extends "res://addons/simple_vn/screen_effects/screen_effect.gd"
+
+@export var default_time = 0.5
 
 @export var saturation := 1.0:
 	set(s):
 		saturation = s
 		$ColorRect.material.set_shader_param("saturation", saturation)
 
-@export var default_time = 0.5
-
 func _get_tool_buttons():
 	return [to_gray, to_color, to_color2, to_color4]
-
-var _tween: Tween
-func _create_tween():
-	if _tween:
-		_tween.kill()
-	_tween = get_tree().create_tween()
-	return _tween
 
 func to_color(time := default_time):
 	_create_tween().tween_property(self, "saturation", 1.0, time)
