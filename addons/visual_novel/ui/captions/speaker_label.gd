fd395185c9b74aec2aefbb32308ea8a235c28ebc
teebarjunk <teebarjunk@github.com>
2022-04-13 23:16:04 -0400

    Latest.

--- addons/visual_novel/ui/captions/speaker_label.gd
+++ addons/visual_novel/ui/captions/speaker_label.gd
@@ -2,14 +2,38 @@
 extends RichTextLabel2
 
 @export var disabled := false
+@export var active := false
+
+@export var head := "[dim;lb]":
+	set(x):
+		head = x
+		_redraw()
+		
+@export var tail := "[dim;rb]":
+	set(x):
+		tail = x
+		_redraw()
 
 func _ready() -> void:
 	add_to_group("@.speaker_label")
-	Dialogue.caption.connect(_caption)
+	VisualNovel.caption_started.connect(_caption_started)
+	VisualNovel.caption_ended.connect(_caption_ended)
+	clear()
 
-func _caption(speaker: String, caption: String, kwargs := {}):
+func _preparse(t: String) -> String:
+	return super._preparse(head + t + tail)
+
+func _caption_started(speaker: String, caption: String, kwargs := {}):
 	if disabled:
 		return
+	
+	if speaker:
+		active = true
+		set_bbcode(speaker)
+
+func _caption_ended():
+	clear()
+	active = false
 
 func speaker_label(id: String, kwargs := {}):
 	disabled = id != name
